6cb39f67b2bc0b68348ea690f7110930c6706a7b
martin-otero <77897569+martin-otero@users.noreply.github.com>
2021-03-15 21:47:50 +0100

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -1,7 +1,7 @@
-% cd to the directory of installation
+#cd to the directory of installation
 
-% git clone https://github.com/martin-otero/Photo-Command
+git clone https://github.com/martin-otero/Photo-Command
 
-% cd Photo-Command
+cd Photo-Command
 
-% python3 Photo-Command.py
+python3 Photo-Command.py
